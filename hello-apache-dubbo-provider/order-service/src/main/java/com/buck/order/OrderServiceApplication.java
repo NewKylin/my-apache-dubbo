de960e92e3cf932f02c2f9fb0523d80d80ca3c73
yan xun <941141753@qq.com>
2020-09-16 16:52:09 +0800

    test

--- hello-apache-dubbo-provider/order-service/src/main/java/com/buck/order/OrderServiceApplication.java
+++ hello-apache-dubbo-provider/order-service/src/main/java/com/buck/order/OrderServiceApplication.java
@@ -1,7 +1,9 @@
 package com.buck.order;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @program: hello-apache-dubbo
@@ -10,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create: 2020-09-14 11:16
  **/
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.buck.order.mapper")
 public class OrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class);
--- hello-apache-dubbo-provider/product-service/src/main/java/com/buck/product/ProductServiceApplication.java
+++ hello-apache-dubbo-provider/product-service/src/main/java/com/buck/product/ProductServiceApplication.java
@@ -1,5 +1,6 @@
 package com.buck.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  **/
 
 @SpringBootApplication
+@MapperScan("com.buck.product.mapper")
 public class ProductServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class);
